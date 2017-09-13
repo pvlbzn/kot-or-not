7a44289363e0aae190146048ff62b330287f6b49
Pavel Bazin <pvlbzn@gmail.com>
2017-09-13 15:39:30 +0200

    Initial commit

---
+++ algorithms.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def propagate(weights, bias, data, labels):
+
+    m = data.shape[1]
+
+    # Forward propagation
+    activation = sigmoid(np.dot(weights.T, data) + bias)
+    cost = (-1 / m) * np.sum(labels * np.log(activation) + (1 - labels) * np.log(1 - activation))
+    
+    # Backward propagation
+    weight_derivative = (1 / m) * (np.dot(data, (activation - labels).T))
+    bias_derivative   = (1 / m) * np.sum(activation - labels)
+
+    cost = np.squeeze(cost)
+    grads = {'dw': weight_derivative, 'db': bias_derivative}
+
+    return grads, cost
+
+
+def optimize(weights, bias, data, labels, niter, lrate, verbose=False):
+    costs = []
+
+    for i in range(niter):
+        grads, cost = propagate(weights, bias, data, labels)
+
+        weight_derivative = grads['dw']
+        bias_derivative   = grads['db']
+
+        weights = weights - lrate * weight_derivative
+        bias    = bias    - lrate * bias_derivative
+
+        # Record progress of costs (each 100th)
+        if i % 100 == 0:
+            costs.append(cost)
+            if verbose:
+                print(f'i: {i}\tcost: {cost}')
+        
+    return weights, bias, weight_derivative, bias_derivative, costs
+
+
+def predict(weights, bias, data):
+    m = data.shape[1]
+    prediction = np.zeros((1, m))
+    weights = weights.reshape(data.shape[0], 1)
+
+    activation = sigmoid(np.dot(weights.T, data) + bias)
+
+    for i in range(activation.shape[1]):
+        # prediction[0][i] = 0 if activation[0][i] <= 0.5 else 1
+        if activation[0][i] <= 0.5:
+            prediction[0][i] = 0
+        else:
+            prediction[0][i] = 1
+
+    return prediction, activation
+
+
+def model(data, labels, niter, lrate, verbose):
+    weight = np.zeros(shape=(data.shape[0], 1))
+    bias   = 0
+
+    weights, bias, weight_derivative, bias_derivative, costs = optimize(weight, bias, data, labels, niter, lrate, verbose)
+
+    prediction = predict(weights, bias, data)
+    
+    accuracy = 100 - np.mean(np.abs(prediction - labels)) * 100
+
+    if verbose:
+        print(f'accuracy: {accuracy}')
+    
+    return costs, prediction, weights, bias, lrate, niter
+    
---
+++ main.py
@@ -0,0 +1,57 @@
+import h5py
+import algorithms
+
+import numpy as np
+from scipy import ndimage, misc
+
+num_px = 0
+
+
+def load_data():
+    dataset = h5py.File('./data/train_catvnoncat.h5', 'r')
+
+    data = np.array(dataset['train_set_x'][:])
+    labels = np.array(dataset['train_set_y'][:])
+
+    labels = labels.reshape((1, labels.shape[0]))
+
+    global num_px
+    num_px = data[0].shape[0]
+
+    return data, labels
+
+
+def process_image(fname, weigths, bias):
+    fname = 'images/' + fname
+    img = np.array(ndimage.imread(fname, flatten=False))
+    res = misc.imresize(
+        img, size=(num_px, num_px)).reshape((1, num_px * num_px * 3)).T
+
+    prediction, activation = algorithms.predict(weigths, bias, res)
+
+    return prediction, activation
+
+
+def main():
+    data, labels = load_data()
+
+    # Dataset standardization
+    flatten_data = data.reshape(data.shape[0], -1).T
+    training_set = flatten_data / 255.0
+
+    # Train
+    costs, prediction, weights, bias, lrate, niter = algorithms.model(
+        training_set, labels, niter=5000, lrate=0.01, verbose=True)
+
+    # 2000,  .005, 99.04306220095694, 0.14087207570310153
+    # 5000,  .01,  100.0,             0.03149755317976998
+    # 1000,  .01,  98.56459330143541, 0.12497148000976802
+
+    print(process_image('r1.jpg', weights, bias))
+    print(process_image('r2.jpg', weights, bias))
+    print(process_image('r3.jpg', weights, bias))
+    print(process_image('r4.jpg', weights, bias))
+
+
+if __name__ == '__main__':
+    main()
